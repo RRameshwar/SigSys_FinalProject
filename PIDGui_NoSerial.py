cfdbc9ef4239e90084e0b57639f60a319eb8a291
mtkwock <m.t.kwock@gmail.com>
2015-04-23 12:21:58 -0400

    Now has naive PID control arduino code
    Not sure if Serial Works.  Needs more Serial.print debug statements.
    scaleFactorP/I/D need to be tuned so that our PIDgui values work nicely.
    Crying...  lots of crying...

---
+++ PIDGui_NoSerial.py
@@ -0,0 +1,164 @@
+from pygame.locals import *
+import pygame, os, sys
+import serial
+import time
+pygame.init()
+# ser = serial.Serial(0)
+
+WIDTH = 640
+HEIGHT = 480
+
+
+class sliderBack():
+    def __init__(self,name,x, y, width, height):
+        self.name = name
+        self.view = pygame.Surface((width, height))
+        self.rect = self.view.get_rect()
+        
+        self.view.fill((0,255,0))
+
+        self.rect.topleft = (x, y)
+
+class sliderBar():
+    def __init__(self,name,x, y, width, height):
+        self.name = name
+        self.view = pygame.Surface((width, height))
+        self.rect = self.view.get_rect()
+        
+        self.view.fill((255,0,0))
+        self.rect.midleft = (x,y)
+
+class Button():
+    def __init__(self, name, x, y, width, height):
+        self.name = name
+        self.view = pygame.Surface((width, height))
+        self.rect = self.view.get_rect()
+        
+        self.view.fill((0,0,255))
+
+        self.rect.topleft = (x, y)
+
+P = 0
+I = 0
+D = 0
+
+scr = pygame.display.set_mode((WIDTH, HEIGHT))
+
+pygame.display.set_caption('Box Test')
+
+backs = []
+bars = []
+labels = []
+
+
+back1 = sliderBack("ProportionalBack", 30,HEIGHT/4, WIDTH-200, 50)
+back2 = sliderBack("IntegralBack", 30, 2*HEIGHT/4, WIDTH-200, 50)
+back3 = sliderBack("IntegralBack", 30, 3*HEIGHT/4, WIDTH-200, 50)
+
+bar1 = sliderBar("ProportionalBar", 30, back1.rect.centery, 25, 60)
+bar2 = sliderBar("IntegralBar", 30, back2.rect.centery, 25, 60)
+bar3 = sliderBar("DerivativeBar", 30, back3.rect.centery, 25, 60)
+
+button = Button("Display", back2.rect.topright[0] + 100, back2.rect.topright[1], 64, 64)
+
+myfont = pygame.font.Font(None, 50)
+bigfont = pygame.font.Font(None, 50)
+
+
+label_title = bigfont.render("PID Control", 1, (255, 0, 0))
+label_go = myfont.render("GO", 1, (250, 250, 240))
+label_proportional = myfont.render(str(P), 1, (255, 0, 0))
+label_integral = myfont.render(str(I), 1, (255, 0, 0))
+label_derivative = myfont.render(str(D), 1, (255, 0, 0))
+label_P = myfont.render("P", 1, (255, 250, 240))
+label_I = myfont.render("I", 1, (255, 250, 240))
+label_D = myfont.render("D", 1, (255, 250, 240))
+
+backs.append(back1)
+backs.append(back2)
+backs.append(back3)
+
+bars.append(bar1)
+bars.append(bar2)
+bars.append(bar3)
+
+clicked = False
+
+while True:
+    pygame.display.update()
+    scr.fill((0, 0, 0))
+    
+    for b in backs:
+        scr.blit(b.view, b.rect)
+
+    for b in bars:
+        scr.blit(b.view, b.rect)
+
+    scr.blit(button.view, button.rect)
+    
+    scr.blit(label_proportional, (back1.rect.midright[0] + 10,back1.rect.midright[1]-20))
+    scr.blit(label_integral, (back2.rect.midright[0] + 10,back2.rect.midright[1]-20))
+    scr.blit(label_derivative, (back3.rect.midright[0] + 10,back3.rect.midright[1]-20))
+    scr.blit(label_go, (button.rect.topleft[0] + 5 , button.rect.topleft[1] + 10))
+
+    scr.blit(label_P, (0, back1.rect.topleft[1]+ 10))
+    scr.blit(label_I, (0, back2.rect.topleft[1]+ 10))
+    scr.blit(label_D, (0, back3.rect.topleft[1]+ 10))
+
+    scr.blit(label_title, (WIDTH/2 - 100,0))
+  
+
+    if pygame.mouse.get_pressed()[0]!=0:
+
+        if button.rect.collidepoint(pygame.mouse.get_pos()) and clicked == False:
+            clicked = True
+            print "P = " + str(P) + "\nI = " + str(I) + "\nD = " + str(D)
+            # ser.write(str(P)+str(I)+str(D))
+
+        if not (button.rect.collidepoint(pygame.mouse.get_pos())) and clicked == True:
+            clicked = False            
+
+        else:
+            for i in range (len(bars)):
+                bar = bars[i]
+                rect = bar.rect
+                rect_back = backs[i].rect
+                
+                if rect.collidepoint(pygame.mouse.get_pos()):
+                    rect.centerx = pygame.mouse.get_pos()[0]
+                    if rect.midright[0] > rect_back.midright[0]:
+                        rect.midright = (rect_back.midright)
+                    if rect.midleft[0] < rect_back.midleft[0]:
+                        rect.midleft = (rect_back.midleft)
+
+                pos = rect.centerx
+
+
+                if bar.name == "ProportionalBar":
+                    rect = bar.rect
+                    if not (rect.x == P):
+                        P = rect.x - 30
+                        label_proportional = myfont.render(str(P), 1, (255,0,0))
+                        #print "P = " + str(P)
+                if bar.name == "IntegralBar" and rect.x != I:
+                    rect = bar.rect
+                    if not (rect.x == I):
+                        I = rect.x - 30
+                        label_integral = myfont.render(str(I), 1, (255,0,0))
+                        #print "I = " + str(I)
+                if bar.name == "DerivativeBar" and rect.x != D:
+                    rect = bar.rect
+                    if not (rect.x == D):
+                        D = rect.x - 30
+                        label_derivative = myfont.render(str(D), 1, (255,0,0))
+
+
+        
+
+        
+
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
---
+++ pid_control/pid_control.ino
@@ -0,0 +1,129 @@
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+#include "utility/Adafruit_PWMServoDriver.h"
+
+// Initialize the shield
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+
+// Begin motor 1 on the shield.  Usage below
+// motor->setSpeed(n), 0 <= n <= 255
+// motor->run(FORWARD/BACKWARD), changes direction
+Adafruit_DCMotor *motor = AFMS.getMotor(1);
+int sensorPin = A0;
+
+// PID constants that can be changed using Serial
+// For example, use "P231" to change P to 231.
+// NOTE: cannot set to negative yet
+int kP;
+int kI;
+int kD;
+
+// Scales P, I, and D into proper range so 0 <= p + i + d < 256
+double scaleFactorP;
+double scaleFactorI;
+double scaleFactorD;
+
+// Variables used for calculating P, I, and D
+boolean runForward; // Controls overall direction of P, I, and D.  Change if going wrong way
+int accumulator;    // Integrates the error, is negated when direction is switched
+int lastRead;       // Used for D control and direction control 
+int maxRead;        // Used for Proportional and Integral Control
+double dt;          // Difference in time between calculations
+
+int delayMils;
+
+void setup(){
+  pinMode(sensorPin, INPUT);
+  Serial.begin(9600);
+  AFMS.begin();
+
+  // Initialize values
+  kP = 100; 
+  kI = 0; 
+  kD = 0; 
+  lastRead = 0;
+  maxRead = 500;
+  accumulator = 0;
+  runForward = true;
+  dt = 1.0 / 1000;
+  delayMils = (int)(dt * 1000);
+  scaleFactorP = 1.0/1000;
+  scaleFactorI = 1.0/1000;
+  scaleFactorD = -1.0/1000;
+}
+
+int getSerialNum(){
+  int val = 0;
+  int zeroByte = (int)('0');
+  while (Serial.peek() >= (int)('0') && Serial.peek() <= (int)('9')){
+    val = val * 10 + ((int)(Serial.read()) - zeroByte);
+  }
+  Serial.flush();
+  return val;
+}
+
+void parseSerial(){
+  char incomingByte = Serial.read();
+  
+  switch(incomingByte){
+    case 'P':
+      kP = getSerialNum();
+    break;
+    case 'I':
+      kI = getSerialNum();
+    break;
+    case 'D':
+      kD = getSerialNum();
+    break;
+    default: 
+      Serial.print("Input not understood: ");
+      Serial.println(incomingByte);
+  }
+  
+}
+
+int error(int readVal){
+  // Get error
+  if (readVal > maxRead){
+    
+    maxRead = readVal + 1;
+  }
+  return maxRead - readVal;
+}
+
+void switchDirection(){
+  runForward = !runForward;
+  if(runForward) 
+    motor->run(FORWARD);
+  else 
+    motor->run(BACKWARD);
+}
+
+void loop(){
+  if(Serial.available())
+    parseSerial();
+
+  int thisRead = analogRead(sensorPin);
+  int diff = thisRead - lastRead;
+
+  if(diff < 0){
+    // Account for change in direction.
+    // by switching direction, p is automatically reversed
+    // The integration needs to be reversed
+    switchDirection();
+    accumulator *= -1;
+    diff *= -1;
+  }
+  
+  int err = error(thisRead);
+  double p = kP * err * scaleFactorP;
+  accumulator += err * dt;
+  double i = kI * accumulator * scaleFactorI;
+  double d = kD * diff / dt * scaleFactorD;
+    
+  int drive = (int)(p + i + d);
+  motor->setSpeed(drive);
+  
+  lastRead = thisRead;
+  delay(delayMils);
+}
