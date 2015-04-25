fbf7a368b3738d8ba74483b3838c477b47af7c79
mtkwock <m.t.kwock@gmail.com>
2015-04-25 19:58:39 -0400

    Quite a bit of commenting added with some adjustments to initial numbers and Serial reading/writing

--- pid_control/pid_control.ino
+++ pid_control/pid_control.ino
@@ -25,7 +25,7 @@ double scaleFactorD;
 
 // Variables used for calculating P, I, and D
 boolean runForward; // Controls overall direction of P, I, and D.  Change if going wrong way
-int accumulator;    // Integrates the error, is negated when direction is switched
+double accumulator;    // Integrates the error, is negated when direction is switched
 int lastRead;       // Used for D control and direction control 
 int maxRead;        // Used for Proportional and Integral Control
 double dt;          // Difference in time between calculations
@@ -34,53 +34,75 @@ int delayMils;
 boolean printOn = true;
 
 void setup(){
+  // Pin and system setup code
   pinMode(sensorPin, INPUT);
   Serial.begin(9600);
   AFMS.begin();
 
   // Initialize values
+  // Adjusted directly via Serial
   kP = 100; 
   kI = 0; 
   kD = 0; 
+
   lastRead = 0;
   maxRead = 500;
   accumulator = 0;
   runForward = true;
-  dt = 1.0 / 1000;
+
+  // Defines delay time.  Higher dt makes for lower resolution
+  dt = 1.0 / 50;
   delayMils = (int)(dt * 1000);
-  scaleFactorP = 1.0/1000;
-  scaleFactorI = 1.0/1000;
+  
+  // Change these so that the ranges input by the GUI range well.
+  scaleFactorP = 1.0/100;
+  scaleFactorI = 1.0/200000;
   scaleFactorD = -1.0/1000;
 }
 
-int getSerialNum(){
+// Keeps reading the Serial values until it doesn't see a number.
+// Helper function to understand.
+// ONLY SEES INTEGERS.
+int getSerialNum(){  
   int val = 0;
-  int zeroByte = (int)('0');
+  int zeroByte = (int)('0'); // Uses as a base to convert a character to a number
+  
+  // Keep going as long as it sees an integer
   while (Serial.peek() >= (int)('0') && Serial.peek() <= (int)('9')){
     val = val * 10 + ((int)(Serial.read()) - zeroByte);
   }
-  Serial.flush();
   return val;
 }
 
-void parseSerial(){
-  char incomingByte = Serial.read();
+// This function is called every time a Serial Event happens
+// It reacts to any P, I, or D changes sent through Serial.
+// Must be in form (P/I/D)##### with no spaces and Natural Numbers
+// e.g. P100, I40, D2
+void serialEvent(){
+  Serial.flush(); // Ignores its own Serial Writes
+
+  char incomingByte = Serial.read(); // Get first Serial character to see what to do
   
   switch(incomingByte){
     case 'P':
       kP = getSerialNum();
-      Serial.println("kP set to " + kP);
+      Serial.print("kP set to ");
+      Serial.println(kP, DEC);
     break;
     case 'I':
       kI = getSerialNum();
-      Serial.println("kI set to " + kP);
+      Serial.print("kI set to ");
+      Serial.println(kI, DEC);
     break;
     case 'D':
       kD = getSerialNum();
-      Serial.println("kD set to " + kP);
+      Serial.print("kD set to ");
+      Serial.println(kD, DEC);
     break;
+    // Using a single lowercase p switches the print off.  Just convenience
     case 'p':
       switchPrint();
+      break;
     default: 
       Serial.print("Input not understood: ");
       Serial.println(incomingByte);
@@ -88,15 +110,17 @@ void parseSerial(){
   
 }
 
-int error(int readVal){
-  // Get error
-  if (readVal > maxRead){
-    
-    maxRead = readVal + 1;
+// Get the understood "Error" of the system
+// Error is equal to HIGHEST_READ - CURRENT_READ
+// Highest read is set to current read if current read is greater
+int error(int readVal){ 
+  if (readVal > maxRead){    
+    maxRead = readVal;
   }
   return maxRead - readVal;
 }
 
+// Switches motor direction and keeps memory of current direction
 void switchDirection(){
   runForward = !runForward;
   if(runForward) 
@@ -105,10 +129,12 @@ void switchDirection(){
     motor->run(BACKWARD);
 }
 
+// Switches printing to serial on and off.
 void switchPrint(){
   printOn = !printOn;
 }
 
+// Print function for pid.  Doesn't print if printOn is false
 void printVals(double p, double i, double d){
   if(!printOn) return;
 
@@ -116,16 +142,17 @@ void printVals(double p, double i, double d){
   int printI = (int) i;
   int printD = (int) d;
   int drive = (int) (p + i + d);
-  Serial.print( "P: " + printP);
-  Serial.print(" I: " + printI);
-  Serial.print(" D: " + printD);
-  Serial.println(" Total: " + drive);
+  Serial.print( "P: ");
+  Serial.print(p, 2);
+  Serial.print(" I: ");
+  Serial.print(i, 2);
+  Serial.print(" D: ");
+  Serial.print(d, 2);
+  Serial.print(" Total: ");
+  Serial.println(p + i + d, 3);
 }
 
 void loop(){
-  if(Serial.available())
-    parseSerial();
-
   int thisRead = analogRead(sensorPin);
   int diff = thisRead - lastRead;
 
@@ -138,16 +165,24 @@ void loop(){
     diff *= -1;
   }
   
+  // Calculate PID
   int err = error(thisRead);
   double p = kP * err * scaleFactorP;
   accumulator += err * dt;
   double i = kI * accumulator * scaleFactorI;
   double d = kD * diff / dt * scaleFactorD;
+  
+  // Ensure D <0
+  if(d > 0)
+    d *=-1;
     
+  // Set Motor speed according to PID
   int drive = (int)(p + i + d);
   printVals(p, i, d);
+  constrain(drive, 0, 255);
   motor->setSpeed(drive);
   
+  // Update and Loop
   lastRead = thisRead;
   delay(delayMils);
 }
